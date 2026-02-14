0675953170b027ffebfdbbc942aa78ffecde6808
GabrielGarciaToriz <gabrielgtoriz@hotmail.com>
2026-02-13 18:25:04 -0600

    Validation Client Email, Password

--- src/main/resources/static/js/Validaciones/Correo.js
+++ src/main/resources/static/js/Validaciones/Correo.js
@@ -1,14 +1,15 @@
 import { limpiarEstilos, marcarExito, mostrarError } from "../Helpers/HelpersUI.js";
 //EXPRESION REFULAR PARA VALIDAR EL CORREO
 const regexCorreo = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
+const msgError = "El correo no tiene el formato correcto";
+const msgCorrect = "El correo tiene el formato correcto";
 
 export function validarCorreo(input, event) {
     var correo = $(input).val().trim().toLowerCase() + event.key;
     if (regexCorreo.test(correo)) {
-        marcarExito(input)
+        marcarExito(input, msgCorrect)
     } else {
-        mostrarError(input,"No es un correo valido")
+        mostrarError(input, msgError)
     }
 
 }
@@ -20,9 +21,9 @@ export function validarCorreoBlur(input) {
         return;
     }
     if (regexCorreo.test(correo)) {
-        marcarExito(input, "El correo es correcto")
+        marcarExito(input, msgCorrect)
     } else {
-        mostrarError(input, "No es un correo valido")
+        mostrarError(input, msgError)
     }
 
 }
--- src/main/resources/static/js/Validaciones/Password.js
+++ src/main/resources/static/js/Validaciones/Password.js
@@ -1,12 +1,13 @@
 import { limpiarEstilos, marcarExito, mostrarError } from "../Helpers/HelpersUI.js";
-const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
-
+const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,16}$/;
+const msgError = "La contraseña no cumple los parametros";
+const msgCorrect = "La contraseña cumple los parametros";
 export function validarPassword(input, event) {
     var password = $(input).val().trim() + event.key;
     if (regexPassword.test(password)) {
-        marcarExito(input)
+        marcarExito(input, msgCorrect)
     } else {
-        mostrarError(input)
+        mostrarError(input, msgError)
     }
 }
 
@@ -16,8 +17,8 @@ export function validarPasswordBlur(input) {
         limpiarEstilos(input)
     }
     if (regexPassword.test(password)) {
-        marcarExito(input)
+        marcarExito(input, msgCorrect)
     } else {
-        mostrarError(input)
+        mostrarError(input, msgError)
     }
 }
