f49fb9692ef3dbb3faae40a90afe5ebfed9b0917
GabTrax <g.garcia.t839@edu.utc.mx>
2026-03-02 15:47:00 -0600

    JPA USUARIO

--- src/main/java/com/digis01/GGarciaProgramacionNCapasMaven/JPA/Usuario.java
+++ src/main/java/com/digis01/GGarciaProgramacionNCapasMaven/JPA/Usuario.java
@@ -1,5 +1,45 @@
 package com.digis01.GGarciaProgramacionNCapasMaven.JPA;
 
-public class Usuario {
+import jakarta.persistence.*;
+import com.digis01.GGarciaProgramacionNCapasMaven.JPA.Rol;
+import com.digis01.GGarciaProgramacionNCapasMaven.JPA.Direccion;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    private int IdUsuario;
+    private String Nombre;
+    private String ApellidoPaterno;
+    private String ApellidoMaterno;
+    private String Celular;
+    private String Curp;
+    private String UserName;
+    private String Email;
+    private String Password;
+    private String Sexo;
+    private String Telefono;
+    private String FechaNacimineot;
+    @Valid
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idrol")
+    public Rol Rol;
+    @Valid
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<Direccion> Direcciones;
+    private int Estatus;
 }
+
+
+
